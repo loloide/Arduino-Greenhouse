f5eac7a31236f55838a59bd397081c110ba4b978
loloide <lorendlc2005@gmail.com>
2022-01-19 17:17:26 -0300

    fixed bugs with the clock and the window

--- arduino-greenhouse.ino
+++ arduino-greenhouse.ino
@@ -12,9 +12,6 @@
 int s1 = 0;
 int s2 = 0;
 int s3 = 0;
-int s1lcd = 0;
-int s2lcd = 0;
-int s3lcd = 0;
 int wantedtemp = 20;
 int openedWindow = 0;
 
@@ -24,6 +21,7 @@ Servo servo;
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 
 //Ds1302 rtc(PIN_ENA, PIN_CLK, PIN_DAT);
+//remember to set the hour the first time you run the arduino and then delete or comment the command later
 Ds1302 rtc(5, 7, 6);
 
 //DHT dht(pin, model);
@@ -31,7 +29,7 @@ DHT dht0(A0, DHT11);
 DHT dht1(A1, DHT11);
 DHT dht2(A2, DHT11);
 
-void setup()				
+void setup()        
 {
   //start the different modules
   rtc.init();
@@ -43,7 +41,7 @@ void setup()
   dht2.begin();
   Serial.begin(9600);
   
-//we dont need to declare the inputs because we already did in the "DHT dht();" functions
+  //we dont need to declare the inputs because we already did in the "DHT dht();" functions
   
   pinMode(1, INPUT);
   pinMode(2, INPUT);
@@ -53,6 +51,7 @@ void setup()
   pinMode(10, OUTPUT);
   
   servo.attach(9);
+
   
 }
 
@@ -69,20 +68,20 @@ void loop()
     if (s2 < wantedtemp) { //look for hottest
       
       if (s1 > s3) {
-      	//exec s1
+        //exec s1
         digitalWrite(13,HIGH);//HIGH= 5v
       }
       else {
-      	digitalWrite(13,LOW);
+        digitalWrite(13,LOW);
       }
       
       
       if (s3 > s1) {
-      	//exec s3
-      	digitalWrite(12,HIGH);//HIGH= 5v
+        //exec s3
+        digitalWrite(12,HIGH);//HIGH= 5v
       }
       else {
-      	digitalWrite(12,LOW);
+        digitalWrite(12,LOW);
       }
     }
     
@@ -130,18 +129,16 @@ void loop()
   lcd.print(s3c);
   delay(50);
 
+  lcd.setCursor(0,0);
+  lcd.print(openedWindow);
+
+
   //change wantedtemp with the buttons
   if (digitalRead(1) == HIGH) {
     wantedtemp = wantedtemp + 1;
-    Serial.print("+");
-  }else {
-    Serial.print("=");
   }
   if (digitalRead(0) == HIGH) {
     wantedtemp = wantedtemp - 1;
-    Serial.print("-");
-  }else {
-    Serial.print(":");
   }
   delay(200);
 
@@ -149,23 +146,27 @@ void loop()
   rtc.getDateTime(&now);
 
   //move window with the clock
-  if (now.hour < 17 && now.hour < 19) {
-      servo.write(0);
+  if (now.hour > 16 && now.hour < 18) {
+      servo.write(0);//open
       openedWindow = 1;
   }
   else {
     //move window with the temperature
     if (s2 > 20) {
-      servo.write(0);
+      servo.write(0);//open
       openedWindow = 1;
     }
     else if (s2 < 15) {
-      servo.write(180);
+      servo.write(180);//close
       openedWindow = 0;
     }  
     else {
-      servo.write(180);
+      servo.write(180);//close
       openedWindow = 0;
     }
   }
+  Serial.print(now.hour);
+  Serial.print(" ");
+  Serial.print(now.minute);
+  Serial.print("  ");
 }
