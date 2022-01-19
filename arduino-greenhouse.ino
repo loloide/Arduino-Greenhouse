3a8535f9d6703d1d93873d64bd04caf851a996b3
loloide <lorendlc2005@gmail.com>
2022-01-19 18:25:04 -0300

    added custom characters to the lcd

--- arduino-greenhouse.ino
+++ arduino-greenhouse.ino
@@ -12,8 +12,11 @@
 int s1 = 0;
 int s2 = 0;
 int s3 = 0;
+int humid = 0;
 int wantedtemp = 20;
 int openedWindow = 0;
+int openHour = 17;
+int closeHour = 19;
 
 Servo servo;
 
@@ -21,7 +24,6 @@ Servo servo;
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 
 //Ds1302 rtc(PIN_ENA, PIN_CLK, PIN_DAT);
-//remember to set the hour the first time you run the arduino and then delete or comment the command later
 Ds1302 rtc(5, 7, 6);
 
 //DHT dht(pin, model);
@@ -29,6 +31,77 @@ DHT dht0(A0, DHT11);
 DHT dht1(A1, DHT11);
 DHT dht2(A2, DHT11);
 
+byte drop[8] =
+  {
+  0b00100,
+  0b00100,
+  0b01110,
+  0b01110,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b01110
+  };
+
+byte window[8] =
+{
+0b00000,
+0b11111,
+0b10001,
+0b10001,
+0b10001,
+0b10001,
+0b11111,
+0b00000
+};
+
+byte inside[8] =
+{
+0b00000,
+0b00000,
+0b11111,
+0b10001,
+0b10101,
+0b10001,
+0b11111,
+0b00000
+};
+
+byte outside[8] =
+{
+0b00100,
+0b00000,
+0b11111,
+0b10001,
+0b10001,
+0b10001,
+0b11111,
+0b00000
+};
+
+byte hole[8] =
+{
+0b00100,
+0b00100,
+0b00100,
+0b00100,
+0b00100,
+0b11111,
+0b01110,
+0b00100
+};
+
+byte arrows[8] =
+{
+0b00000,
+0b00000,
+0b10001,
+0b11011,
+0b11011,
+0b10001,
+0b00000,
+0b00000
+};
 void setup()        
 {
   //start the different modules
@@ -53,6 +126,12 @@ void setup()
   servo.attach(9);
 
   
+  lcd.createChar(0, drop);
+  lcd.createChar(1, window);
+  lcd.createChar(2, inside);
+  lcd.createChar(3, outside);
+  lcd.createChar(4, hole);
+  lcd.createChar(5, arrows);
 }
 
 void loop()
@@ -61,6 +140,7 @@ void loop()
   s1 = dht0.readTemperature();
   s2 = dht1.readTemperature();
   s3 = dht2.readTemperature();
+  humid = dht1.readHumidity();
   
   
   if (s2 != wantedtemp) {
@@ -109,29 +189,51 @@ void loop()
   //print in the display
   //|s2 wantedtemp|
   //|s1         s3|
+  lcd.setCursor(0,0);
+  lcd.write(2);
+  
   lcd.setCursor(1,0);
-  String s2c = "I:" + String(s2) + String((char)223) + "C ";
+  String s2c = ":" + String(s2) + String((char)223);
   lcd.print(s2c);
   delay(50);
 
-  lcd.setCursor(9,0);
-  String wantedlcd = "D:" + String(wantedtemp) + String((char)223) + "C ";
+  lcd.setCursor(6,0);
+  lcd.write(5);
+  
+  lcd.setCursor(7,0);
+  String wantedlcd = ":" + String(wantedtemp) + String((char)223);
   lcd.print(wantedlcd);
   delay(50);
+
+  lcd.setCursor(6,1);
+  lcd.write(4);
   
-  lcd.setCursor(1,1);
-  String s1c = "H:" + String(s1) + String((char)223) + "C ";
+  lcd.setCursor(7,1);
+  String s1c = ":" + String(s1) + String((char)223);
   lcd.print(s1c);
   delay(50);
+
+  lcd.setCursor(0,1);
+  lcd.write(3);
   
-  lcd.setCursor(9,1); 
-  String s3c = "O:" + String(s3) + String((char)223) + "C ";
+  lcd.setCursor(1,1); 
+  String s3c = ":" + String(s3) + String((char)223);
   lcd.print(s3c);
   delay(50);
 
-  lcd.setCursor(0,0);
-  lcd.print(openedWindow);
+  
+  lcd.setCursor(12,0);
+  lcd.write(0);
+  
+  lcd.setCursor(13,0);
+  String windowLcd = String(humid) + String((char)37);
+  lcd.print(windowLcd);
 
+  lcd.setCursor(12,1);
+  lcd.write(1);
+
+  lcd.setCursor(13,1);
+  lcd.print(openedWindow);
 
   //change wantedtemp with the buttons
   if (digitalRead(1) == HIGH) {
@@ -146,7 +248,7 @@ void loop()
   rtc.getDateTime(&now);
 
   //move window with the clock
-  if (now.hour > 16 && now.hour < 18) {
+  if (now.hour >= openHour && now.hour <= closeHour) {
       servo.write(0);//open
       openedWindow = 1;
   }
@@ -165,8 +267,4 @@ void loop()
       openedWindow = 0;
     }
   }
-  Serial.print(now.hour);
-  Serial.print(" ");
-  Serial.print(now.minute);
-  Serial.print("  ");
 }
